3b86c000660a07806dd3708c4019b0801d111255
Nghia Tran <vinhnghiatran@gmail.com>
2017-05-30 15:21:28 +0700

    Init Project 🔥

---
+++ UberGo/UberGo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UberGo
+//
+//  Created by Nghia Tran on 5/30/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UberGoCore/UberGoCore/UberGoCore.h
@@ -0,0 +1,19 @@
+//
+//  UberGoCore.h
+//  UberGoCore
+//
+//  Created by Nghia Tran on 5/30/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for UberGoCore.
+FOUNDATION_EXPORT double UberGoCoreVersionNumber;
+
+//! Project version string for UberGoCore.
+FOUNDATION_EXPORT const unsigned char UberGoCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UberGoCore/PublicHeader.h>
+
+
