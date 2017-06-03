a06d6489e13408f932dad8d9f402b7a706e190a8
Nghia Tran <vinhnghiatran@gmail.com>
2017-06-03 09:17:04 +0700

    Experiment to show Login View Controller by Popover in MenuBar

--- UberGo/UberGo/AppDelegate.swift
+++ UberGo/UberGo/AppDelegate.swift
@@ -13,18 +13,66 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     // MARK: - Variable
-    
+    fileprivate let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    fileprivate let popover = NSPopover()
     
     // MARK: - Action
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        // Setup
+        self.setupPopover()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+}
 
+// MARK: - Private
+extension AppDelegate {
+    
+    fileprivate func setupPopover() {
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.imagePosition = .imageLeft
+            button.action = #selector(togglePopover)
+        }
+        
+        popover.contentViewController = LoginViewController(nibName: "LoginViewController", bundle: nil)
+        popover.appearance = NSAppearance(named: NSAppearanceNameAqua)
+        popover.animates = false
+        popover.behavior = .transient
+
+    }
+    
+    @objc fileprivate func togglePopover() {
+        if self.popover.isShown {
+            self.close()
+        } else {
+            self.show()
+        }
+    }
+    
+    fileprivate func close() {
+        if self.popover.isShown {
+            return
+        }
+        
+        self.popover.close()
+    }
+    
+    fileprivate func show() {
+        
+        NSRunningApplication.current().activate(options: NSApplicationActivationOptions.activateIgnoringOtherApps)
+        
+        guard let button = self.statusItem.button else {
+            return
+        }
+        
+        self.popover.show(relativeTo: button.frame, of: button, preferredEdge: .minY)
+    }
 }
 
