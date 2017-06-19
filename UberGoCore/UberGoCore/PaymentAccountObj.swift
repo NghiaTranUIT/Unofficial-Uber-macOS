d45e3f23ccf93fb20e22d5fcfbfd4fd65bd8209d
Nghia Tran <vinhnghiatran@gmail.com>
2017-06-20 00:12:25 +0700

    Supported enum PaymentAccountType

--- UberGoCore/UberGoCore/PaymentAccountObj.swift
+++ UberGoCore/UberGoCore/PaymentAccountObj.swift
@@ -8,18 +8,48 @@
 
 import ObjectMapper
 
+public enum PaymentAccountType: String {
+    case airtelMoney = "airtel_money"
+    case alipay
+    case applePay = "apple_pay"
+    case americanExpress = "american_express"
+    case androidPay = "android_pay"
+    case baiduWallet = "baidu_wallet"
+    case businessAccount = "business_account"
+    case familyAccount = "family_account"
+    case cash
+    case discover
+    case jcb
+    case lianlian
+    case maestro
+    case mastercard
+    case paypal
+    case paytm
+    case ucharge
+    case unionpay
+    case unknown
+    case visa
+    case zaakpay
+}
+
 open class PaymentAccountObj: BaseObj {
 
     // MARK: - Variable
     public var paymentMethodId: String?
-    public var type: String?
+    public var typeCode: String?
     public var accountDescription: String?
+    public var type: PaymentAccountType {
+        guard let code = self.typeCode else {
+            return .unknown
+        }
+        return PaymentAccountType(rawValue: code) ?? .unknown
+    }
 
     public override func mapping(map: Map) {
         super.mapping(map: map)
 
         self.paymentMethodId <- map[Constants.Object.PaymentAccount.PaymentMethodId]
-        self.type <- map[Constants.Object.PaymentAccount.Type]
+        self.typeCode <- map[Constants.Object.PaymentAccount.Type]
         self.accountDescription <- map[Constants.Object.PaymentAccount.Description]
     }
 }
