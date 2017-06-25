e29bacd741f3e2ab9eea5aac6423954cc2a634e4
Nghia Tran <vinhnghiatran@gmail.com>
2017-06-25 11:12:12 +0700

    Layout basic trip activity view

---
+++ UberGo/UberGo/TripActivityView.swift
@@ -0,0 +1,19 @@
+//
+//  TripActivityView.swift
+//  UberGo
+//
+//  Created by Nghia Tran on 6/25/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+class TripActivityView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
