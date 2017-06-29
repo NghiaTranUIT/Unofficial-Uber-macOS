dc5a0ce376d9a7814d4edce3f458685a0397d345
Nghia Tran <vinhnghiatran@gmail.com>
2017-06-29 23:10:55 +0700

    Layout Callout Annotation with properly horizontal/vertical hug/compression priority

---
+++ UberGo/UberGo/CalloutAnnotations.swift
@@ -0,0 +1,18 @@
+//
+//  CalloutAnnotations.swift
+//  UberGo
+//
+//  Created by Nghia Tran on 6/29/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+class CalloutAnnotations: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
