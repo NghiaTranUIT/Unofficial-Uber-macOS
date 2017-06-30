bae993962953439619156024834ce363c94b4604
Nghia Tran <vinhnghiatran@gmail.com>
2017-06-30 12:01:53 +0700

    Implemented logic for CalloutAnnotations. Support destination callout annotation

--- UberGo/UberGo/CalloutAnnotations.swift
+++ UberGo/UberGo/CalloutAnnotations.swift
@@ -6,15 +6,66 @@
 //  Copyright © 2017 Nghia Tran. All rights reserved.
 //
 
-import Cocoa
+import UberGoCore
+
+enum CalloutAnnotationsLayoutMode {
+    case pickup
+    case destination
+}
 
 class CalloutAnnotations: NSViewController {
 
+    // MARK: - OUTLET
+    @IBOutlet fileprivate weak var timeContainerViewWidth: NSLayoutConstraint!
+    @IBOutlet fileprivate weak var timeEstimateLbl: NSTextField!
+    @IBOutlet fileprivate weak var addressLbl: NSTextField!
+
+    // MARK: - Variable
+    fileprivate var timeObj: TimeEstimateObj?
+    fileprivate var destinationObj: PlaceObj?
+    fileprivate var layoutMode: CalloutAnnotationsLayoutMode = .pickup
+
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
 
-        self.view.backgroundColor = NSColor.white
+        self.initCommon()
+        self.setupLayout()
+        self.setupData()
     }
 
+    fileprivate func setupLayout() {
+        switch layoutMode {
+        case .pickup:
+            self.timeContainerViewWidth.constant = 36
+            self.view.layoutSubtreeIfNeeded()
+        case .destination:
+            self.timeContainerViewWidth.constant = 0
+            self.view.layoutSubtreeIfNeeded()
+        }
+    }
+
+    fileprivate func setupData() {
+
+        guard let timeObj = self.timeObj else { return }
+        self.timeEstimateLbl.stringValue = "\(timeObj.estimate ?? 5)"
+
+        guard let destObj = self.destinationObj else { return }
+        self.addressLbl.stringValue = destObj.address ?? ""
+    }
+
+    // MARK: - Public
+    public func setupCallout(mode: CalloutAnnotationsLayoutMode, timeObj: TimeEstimateObj?, destinationObj: PlaceObj?) {
+        self.layoutMode = mode
+        self.timeObj = timeObj
+        self.destinationObj = destinationObj
+    }
+}
+
+// MARK: - Private
+extension CalloutAnnotations {
+
+    fileprivate func initCommon() {
+        self.view.backgroundColor = NSColor.white
+    }
 }
--- UberGo/UberGo/DestinationAnnotation.swift
+++ UberGo/UberGo/DestinationAnnotation.swift
@@ -8,10 +8,12 @@
 
 import Foundation
 import Mapbox
+import UberGoCore
 
 class DestinationAnnotation: MGLPointAnnotation {
 
     // MARK: - Variable
+    fileprivate var placeObj: PlaceObj!
 
     fileprivate lazy var _imageAnnotation: MGLAnnotationImage = {
         let image = NSImage(imageLiteralResourceName: "destination_mark")
@@ -20,8 +22,24 @@ class DestinationAnnotation: MGLPointAnnotation {
     }()
 
     fileprivate lazy var _calloutController: NSViewController = {
-        return CalloutAnnotations(nibName: "CalloutAnnotations", bundle: nil)!
+        let controller = CalloutAnnotations(nibName: "CalloutAnnotations", bundle: nil)!
+        controller.setupCallout(mode: .destination, timeObj: nil, destinationObj: self.placeObj)
+        return controller
     }()
+
+    // MARK: - Init
+    public init(placeObj: PlaceObj) {
+        self.placeObj = placeObj
+
+        super.init()
+
+        self.coordinate = placeObj.coordinate2D!
+        self.title = placeObj.name
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
 
 // MARK: - UberAnnotationType
