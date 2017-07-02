e7494dbb0c63f77446e37014998c615f375dba51
Nghia Tran <vinhnghiatran@gmail.com>
2017-07-02 16:47:56 +0700

    Layout Product Detail xib

---
+++ UberGo/UberGo/ProductDetailController.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailController.swift
+//  UberGo
+//
+//  Created by Nghia Tran on 7/2/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+class ProductDetailController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
