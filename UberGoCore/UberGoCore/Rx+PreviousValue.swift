30ff5ec78da4c125f70c50b6b357de5a31680718
Nghia Tran <vinhnghiatran@gmail.com>
2017-08-30 10:41:58 +0700

    Implement UberNotificationViewModel and notify proper Notification depend on current Status. Support distinctUntilChanged mechanism

---
+++ UberGoCore/UberGoCore/Rx+PreviousValue.swift
@@ -0,0 +1,9 @@
+//
+//  Rx+PreviousValue.swift
+//  UberGoCore
+//
+//  Created by Nghia Tran on 8/30/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Foundation
---
+++ UberGoCore/UberGoCore/UberNotificationViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  UberNotificationViewModel.swift
+//  UberGoCore
+//
+//  Created by Nghia Tran on 8/30/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Foundation
