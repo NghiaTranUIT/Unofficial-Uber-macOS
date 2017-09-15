5c294576a81d743203a002fae519fb139c111c89
Nghia Tran <vinhnghiatran@gmail.com>
2017-09-15 22:58:33 +0700

    Implement MenuView layout

---
+++ UberGo/UberGo/MenuView.swift
@@ -0,0 +1,19 @@
+//
+//  MenuView.swift
+//  UberGo
+//
+//  Created by Nghia Tran on 9/15/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+class MenuView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
