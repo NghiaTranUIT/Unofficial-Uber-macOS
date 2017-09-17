7a6aeacffc4c7cd9664e1a541beacaff00ddae8b
Nghia Tran <vinhnghiatran@gmail.com>
2017-09-17 16:17:12 +0700

    Layout ProfileInfoCell and ProfileAvatarCell

---
+++ UberGo/UberGo/ProfileController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileController.swift
+//  UberGo
+//
+//  Created by Nghia Tran on 9/17/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+class ProfileController: NSViewController {
+
+    // MARK: - OUTLET
+    @IBOutlet weak var collectionView: UberCollectionView!
+
+    // MARK: - Variable
+
+    // MARK: - View Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func exitBtnOnTap(_ sender: Any) {
+
+    }
+}
